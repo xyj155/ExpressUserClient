20f76837896f3d3b5538543c951017d11c62a32d
xuyijie <1789780841@qq.com>
2018-08-23 18:11:21 +0800

    添加更新模块 2018-8-23

--- app/src/main/java/com/example/administrator/expressuserclient/App.java
+++ app/src/main/java/com/example/administrator/expressuserclient/App.java
@@ -1,11 +1,19 @@
 package com.example.administrator.expressuserclient;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
+
+import com.example.administrator.expressuserclient.commonUtil.ToastUtil;
+import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import cn.jpush.android.api.JPushInterface;
+
+import static com.example.administrator.expressuserclient.weight.FancyAlertDialog.TAG;
 
 /**
  * Created by Administrator on 2018/7/28.
@@ -26,9 +34,9 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
-        CrashReport.initCrashReport(getApplicationContext(), "f2c794a75c", true);
-        Beta.checkUpgrade();
+        Bugly.init(getApplicationContext(), "f2c794a75c", false);
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
     }
+
 }
